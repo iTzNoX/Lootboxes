ecd0159a600046fac4a2df4a180bd91c99e7f163
iTzNoX <137124012+iTzNoX@users.noreply.github.com>
2023-06-23 13:14:02 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+name = "Lootboxes",
+version = "0.0.1",
+author = "iTzNoX",
+author_email = "timothykropp39@gmail.com",
+description = "A small Lootbox simulator",
+long_description = long_description,
+long_description_content_type = "text/markdown",
+url = "https://github.com/iTzNoX/Lootboxes",
+project_urls = "https://github.com/iTzNoX/Lootboxes", 
+license="MIT",
+python_requires >= "3.11",
+)
+  
