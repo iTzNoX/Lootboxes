02dc7c7289b768621321e42d2a815253032de964
iTzNoX <137124012+iTzNoX@users.noreply.github.com>
2023-06-23 15:26:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,9 @@ description = "A small Lootbox simulator",
 long_description = long_description,
 long_description_content_type = "text/markdown",
 url = "https://github.com/iTzNoX/Lootboxes",
-project_urls = "https://github.com/iTzNoX/Lootboxes", 
+project_urls = {
+"Bug Tracker": "https://github.com/iTzNoX/Lootboxes/issues"
+},
 license="MIT",
 python_requires >= "3.11"
 )
